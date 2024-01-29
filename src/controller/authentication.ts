f71ebddb9a34d2ceb7b8c14b10b3669e50682f8b
prathamdupare <prathamdupare@proton.me>
2024-01-29 12:02:38 +0530

    added deleteUser and UpdateUser

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -21,7 +21,7 @@ export const login = async (req: express.Request, res: express.Response) => {
 
     const expectedHash = authentication(user.authentication.salt, password);
 
-    if (user.authentication.password === expectedHash) {
+    if (user.authentication.password !== expectedHash) {
       res.sendStatus(403);
     }
 
